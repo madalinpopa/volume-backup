5c5c3e17bdbb40961996984cdfad2856ff371fa9
Madalin Popa <contact@madalinpopa.com>
2024-01-21 16:38:22 +0200

    Add azure blob upload manager

--- src/docker_volume_backup/main.py
+++ src/docker_volume_backup/main.py
@@ -7,6 +7,13 @@ from docker import DockerClient
 from docker.errors import NotFound
 from docker.models.containers import Container
 
+from azure.identity import ClientSecretCredential
+from azure.storage.blob import BlobServiceClient
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Docker socket
 DOCKER_HOST_URL = "unix://var/run/docker.sock"
 
@@ -16,11 +23,27 @@ BACKUP_CONTAINER_IMAGE = "busybox"
 # The local path where to store the backup files
 BACKUP_STORAGE = "/Users/madalinpopa/backup"
 
+# ID of a Microsoft Entra application
+AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+
+# ID of the application's Microsoft Entra tenant
+AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
+
+# One of the application's client secrets
+AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+
+# Azure storage account name
+AZURE_STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+
+# Azure storage container
+AZURE_STORAGE_CONTAINER = os.getenv("AZURE_STORAGE_CONTAINER")
+
 
 class DockerManager:
     def __init__(self, client: DockerClient):
         self.client = client
         self.tar_files: list[str] = []
+        self.container: Container | None = None
         self.backup_storage = BACKUP_STORAGE
         self.backup_container_image = BACKUP_CONTAINER_IMAGE
 
@@ -33,12 +56,12 @@ class DockerManager:
     def _get_container_volume_details(self, container_name: str) -> dict:
         """Retrieve volume details of a given container."""
         try:
-            container = self.client.containers.get(container_name)
+            self.container = self.client.containers.get(container_name)
             return {
                 "container_name": container_name,
                 "volumes": [
                     (mount["Name"], mount["Destination"])
-                    for mount in container.attrs["Mounts"]
+                    for mount in self.container.attrs["Mounts"]
                     if mount["Type"] == "volume"
                 ],
             }
@@ -65,7 +88,7 @@ class DockerManager:
         os.makedirs(full_path, exist_ok=True)
         return full_path
 
-    def backup(self, container_name: str):
+    def backup(self, container_name: str) -> None:
         """Backup the specified container."""
         try:
             container_details = self._get_container_volume_details(container_name)
@@ -88,15 +111,50 @@ class DockerManager:
             raise
 
 
+class AzureManager:
+    def __init__(self, blob_service_client: BlobServiceClient):
+        self.blob_service_client = blob_service_client
+        self.container = AZURE_STORAGE_CONTAINER
+
+    def upload(self, filename: str, container_name: str) -> None:
+        tar_file = os.path.basename(filename)
+        blob_name = f"{container_name}/{tar_file}"
+        blob_client = self.blob_service_client.get_blob_client(
+            container=self.container,
+            blob=blob_name,
+        )
+        with open(filename, "rb") as data:
+            blob_client.upload_blob(data)
+            logging.info(f"Uploaded volume for container {container_name}")
+
+
+def get_azure_blob_storage_client() -> BlobServiceClient:
+    account_url = f"https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net"
+    credential = ClientSecretCredential(
+        tenant_id=AZURE_TENANT_ID,
+        client_id=AZURE_CLIENT_ID,
+        client_secret=AZURE_CLIENT_SECRET,
+    )
+    return BlobServiceClient(
+        account_url,
+        credential=credential,
+    )
+
+
 def main():
     """
     This is the main function of the backup application.
     It is responsible for executing the backup process.
     """
     client = DockerClient(base_url=DOCKER_HOST_URL)
+    blob_service_client = get_azure_blob_storage_client()
 
-    with DockerManager(client=client) as manager:
-        manager.backup("nginx")
+    azure_manager = AzureManager(blob_service_client=blob_service_client)
+
+    with DockerManager(client=client) as docker_manager:
+        docker_manager.backup("nginx")
+        for tar in docker_manager.tar_files:
+            azure_manager.upload(tar, docker_manager.container.name)
 
 
 if __name__ == "__main__":
